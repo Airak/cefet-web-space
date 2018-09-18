e5568bc9cf5d4aa69b2a744a994f489a44c3959b
Aluno <matheus.mmag@gmail.com>
2018-09-18 12:03:33 -0300

    primeira pratica de javascript

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,13 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let lista = document.querySelectorAll('.botao-expandir-retrair');
+
+for (let l of lista){
+	l.addEventListener('click', function(e) { 
+		let pEl = e.currentTarget;
+		let parent = pEl.parentElement;
+		parent.classList.toggle('expandido');
+		pEl.innerHTML = '-';
+		} )
+}
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,30 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let image = document.querySelector('#slide');
+let anterior = document.querySelector('#anterior');
+let proximo = document.querySelector('#proximo');
+
+let atual = 0; 
+
+
+anterior.addEventListener('click', function() { 
+		if (atual > 1 ) {
+			atual = atual-1;
+		}
+		else{
+			atual = 4;
+		}	
+		image.src = servidorDasImagens+todasAsImagens[atual];
+	})
+
+proximo.addEventListener('click', function() { 
+		if (atual < 4 ) {
+			atual = atual+1;
+		}
+		else{
+			atual = 0;
+		}	
+		image.src = servidorDasImagens+todasAsImagens[atual];
+	})
